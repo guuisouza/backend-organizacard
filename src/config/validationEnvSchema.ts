da61110f91a2128a1b5c247c01c2fc861abd3cdc
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-07-21 16:05:16 -0300

    feat: implement validation schema for .env file

---
+++ src/config/validationEnvSchema.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export const validationEnvSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'provision')
+    .default('development'),
+  APP_PORT: Joi.number().default(3003).required(),
+  POSTGRES_PORT: Joi.number().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
+});
