3c20468a809bd11fd9ca4619fbb40e7f3df262eb
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-07-23 17:25:13 -0300

    chore: configure env schema to receive ETHEREAL MAIL credentials

--- src/config/validationEnvSchema.ts
+++ src/config/validationEnvSchema.ts
@@ -5,9 +5,13 @@ export const validationEnvSchema = Joi.object({
     .valid('development', 'production', 'test', 'provision')
     .default('development'),
   APP_PORT: Joi.number().default(3003).required(),
+  APP_BASE_URL: Joi.string().required(),
   POSTGRES_HOST: Joi.string().required(),
   POSTGRES_PORT: Joi.number().required(),
   POSTGRES_USER: Joi.string().required(),
   POSTGRES_PASSWORD: Joi.string().required(),
   POSTGRES_DB: Joi.string().required(),
+  ETHEREAL_PORT: Joi.number().required(),
+  ETHEREAL_USER: Joi.string().required(),
+  ETHEREAL_PASSWORD: Joi.string().required(),
 });
