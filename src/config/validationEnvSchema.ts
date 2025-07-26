b1156195d6df467ab26ed9be618421282b87e31f
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-07-26 16:26:59 -0300

    chore: update envSchema

--- src/config/validationEnvSchema.ts
+++ src/config/validationEnvSchema.ts
@@ -16,4 +16,6 @@ export const validationEnvSchema = Joi.object({
   ETHEREAL_PASSWORD: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRES_IN: Joi.string().required(),
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+  GOOGLE_CLIENT_SECRET: Joi.string().required(),
 });
